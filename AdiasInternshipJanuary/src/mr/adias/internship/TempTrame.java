77956a3217b88d519ed0b4ef04168a0c5e880eb7
fatimetou 14 <fatimetoummedlemine@gmail.com>
2019-01-26 14:48:38 +0000

    question 1

---
+++ AdiasInternshipJanuary/src/mr/adias/internship/TempTrame.java
@@ -0,0 +1,23 @@
+package mr.adias.internship;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TempTrame {
+int id ;
+String sep= "|" ;
+int	numh ;
+	 list<float> l[] =new ArrayList<>(20) ;
+
+public TempTrame(int id , int numh ,l[] ) {
+	this.id ; 
+	this.numh ;
+	this.add l[]
+	
+}
+
+
+
+}
+
+
