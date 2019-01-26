c341aeea5836587fa03e02eaee021c36cab30fa8
Abou MAT001 <amedou94@gmail.com>
2019-01-26 14:48:00 +0000

    question 2

--- AdiasInternshipJanuary/src/mr/adias/internship/TempTrame.java
+++ AdiasInternshipJanuary/src/mr/adias/internship/TempTrame.java
@@ -1,7 +1,14 @@
 package mr.adias.internship;
 
+import java.io.EOFException;
 import java.util.List;
 
 public class TempTrame {
 List<Float> trame;
+catch
+(EOFException e) {
+	this=null}
+return this;
+}
+
 }
