6f52ed0dde5854fdd2b70e8eb0b1ff9ae239ea56
Boubou Abdoulaye BALL 007 <ballboubou1@gmail.com>
2019-01-26 14:19:28 +0000

    question 2

--- AdiasInternshipJanuary/src/mr/adias/internship/TempTrame.java
+++ AdiasInternshipJanuary/src/mr/adias/internship/TempTrame.java
@@ -1,8 +1,23 @@
 package mr.adias.internship;
 
+
+
 public class TempTrame {
 	
-	
+	@SuppressWarnings("deprecation")
+	public TempTrame MethodeUne(String trame){
+		
+		try {
+			return TempTrame.class.newInstance();
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;	
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
