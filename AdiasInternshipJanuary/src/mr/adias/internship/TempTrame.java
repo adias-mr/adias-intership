89d7306e03db0de3a3695ef3e1733de4564ddb01
Mohamed Val MAT023 <medvallbenvall@gmail.com>
2019-01-26 14:43:36 +0000

    question 2

--- AdiasInternshipJanuary/src/mr/adias/internship/TempTrame.java
+++ AdiasInternshipJanuary/src/mr/adias/internship/TempTrame.java
@@ -2,4 +2,18 @@ package mr.adias.internship;
 
 public class TempTrame {
 
-}
+	public int nbre ;
+	public int Hre ;
+	public float variant ;
+
+
+public static TempTrame Itt(TempTrame TT) {
+	
+TempTrame temptrame = new TempTrame();
+
+       return temptrame ;        
+	
+              }
+
+
+ }
