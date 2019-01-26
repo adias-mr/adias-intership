a747dd380361433c4ac62ad78a5a111b314374e0
Memine MAT018 <memine.diyah@yahoo.fr>
2019-01-26 14:34:28 +0000

    Question 1

---
+++ AdiasInternshipJanuary/src/mr/adias/internship/TempTrame.java
@@ -0,0 +1,12 @@
+package mr.adias.internship;
+
+public class TempTrame {
+	String objetId;
+	int numHeure;
+	String listPrelev;
+	
+	public String trame() {
+		return objetId + "|" + numHeure + "|" +listPrelev;
+	}
+	
+}
