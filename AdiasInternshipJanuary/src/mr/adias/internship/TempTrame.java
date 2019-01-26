d392923f78d7620be42a8d480590347970a4a614
Aliyene Sid'Ahmed Denna <aliounesidahmed@gmail.com>
2019-01-26 14:44:15 +0000

    final

--- AdiasInternshipJanuary/src/mr/adias/internship/TempTrame.java
+++ AdiasInternshipJanuary/src/mr/adias/internship/TempTrame.java
@@ -2,6 +2,35 @@ package mr.adias.internship;
 
 public class TempTrame {
 
+	String idObjet;
+	String heur;
+	String[] ListePrelevements;
 
+
+	public String getIdObjet(String trame) {
+		idObjet="";
+		for(int i=0;i<trame.indexOf("|");i++){
+			idObjet.concat(trame.charAt(i));
+		}
+		return idObjet;
+	}
+
+	public String getHeur(String trame ) {
+		heur="";
+		for(int i=trame.indexOf("|");i<trame.indexOf("|", trame.indexOf("|") + 2);i++){
+			heur.concat(trame.charAt(i));
+		}
+		return heur;
+	}
+
+	public String[] getListePrelevements(String trame) {
+
+		return ListePrelevements;
+	}
+
+	public  TempTrame(String trame){
+		this.trame=trame;
+
+	}
 
 }
