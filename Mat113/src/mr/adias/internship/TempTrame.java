cc9b2ceec22c11502c1f9903b47c292e55f6e025
fatimetou 013 <fatimetou.ismail05@gmail.com>
2019-01-26 14:48:12 +0000

    question 3

--- Mat113/src/mr/adias/internship/TempTrame.java
+++ Mat113/src/mr/adias/internship/TempTrame.java
@@ -10,5 +10,43 @@ package mr.adias.internship;
  * @author DELL
  */
 public class TempTrame {
+    String Type_de_codage, Nombre_de_position, Description;
+
+    public String getType_de_codage() {
+        return Type_de_codage;
+    }
+
+    public void setType_de_codage(String Type_de_codage) {
+        this.Type_de_codage = Type_de_codage;
+    }
+
+    public String getNombre_de_position() {
+        return Nombre_de_position;
+    }
+
+    public void setNombre_de_position(String Nombre_de_position) {
+        this.Nombre_de_position = Nombre_de_position;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String Description) {
+        this.Description = Description;
+    }
+
+    public TempTrame() {
+    }
+    
+    public TempTrame meth2(String Temp){
+        TempTrame t =new TempTrame();
+        return t;
+    }
+    public void method3(TempTrame[] listTem){
+        String chaine;
+        
+        
+    }
     
 }
