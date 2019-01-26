f0ea03d6a8ad948113079d532c3aa1608e10edff
AHMEDKHERCHI004 <ahmed.kherchi1@gmail.com>
2019-01-26 14:40:05 +0100

    question 1

---
+++ AdiasInternshipJanuary/src/mr/adias/internship/TempTrame.java
@@ -0,0 +1,14 @@
+package mr.adias.internship;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TempTrame extends AdiasInternshipJanuary {
+
+	
+	List<String> trames =new ArrayList();
+
+
+System.out.print("Typedecodage"+ trames);
+
+}
